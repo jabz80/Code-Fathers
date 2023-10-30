668b4fb44fac2e74c3b53e0916a84a4fb061feb1
AnoopBhandal <anoopbhandal1@gmail.com>
2023-10-30 11:19:33 +0000

    More work on calendar

--- client/src/components/Calendar/index.jsx
+++ client/src/components/Calendar/index.jsx
@@ -1,29 +1,39 @@
 import React, {useState} from 'react';
 import Calendar from 'react-calendar';
 
+// export default function CalendarApp({date, setDate, title, setTitle, description, setDescription, message, setMessage }){
 export default function CalendarApp(){
-    const [date, useDate] = useState(new Date())
-    const [input, setInput] = useState('')
+    const [date, setDate] = useState(new Date())
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [message, setMessage] = useState('')
+
 
     function handleChange(e){
-        useDate(e)
+        setDate(e)
     }
 
-    function handleInput(e){
+    function handleTitleInput(e){
         const newInput = e.target.value
-        setInput(newInput)
+        setTitle(newInput)
+    }
+
+    function handleDescriptionInput(e){
+        const newInput = e.target.value
+        setDescription(newInput)
     }
 
     function handleSubmit(e){
         e.preventDefault()
-        console.log(input)
+        console.log(title, description)
         // Add event function using input value here
-        if (input.length > 0) {
+        if (title.length > 0 && description.length > 0) {
             fetch('http://localhost:3000/calendar', {
                 method: 'POST',
                 body: JSON.stringify({
-                    date: date,
-                    event: input
+                    task_title: title,
+                    task_description: description,
+                    task_date: date
                 }),
                 headers: {
                     'Content-type': 'application/json; charset=UTF-8',
@@ -43,7 +53,7 @@ export default function CalendarApp(){
                     setMessage('')
                 }, 5000)
             });
-            setInputText('')
+            setTitle('')
             setDescription('')
         } else {
             setMessage('Please enter an event.');
@@ -58,8 +68,14 @@ export default function CalendarApp(){
         <Calendar onChange={handleChange} value={date} />
         <p>Selected date is {date.toLocaleDateString()}</p>
         <form onSubmit={handleSubmit}>
-            <input type='text' onChange={handleInput} value={input} required />
+            <label htmlFor='title'>Add event title here:</label>
+            <input type='text' onChange={handleTitleInput} id='title'required />
+            <br></br>
+            <label htmlFor='event'>Add event description here:</label>
+            <input type='text' onChange={handleDescriptionInput} id='event'/>
+            <br></br>
             <input type='submit' value="Add Event" />
+            <p className='message'>{message}</p>
         </form>
         </>
     )
--- client/src/pages/CalendarPage/index.jsx
+++ client/src/pages/CalendarPage/index.jsx
@@ -4,6 +4,7 @@ import { CalendarApp } from '../../components'
 export default function index() {
   return (
     <>
+    {/* <CalendarApp date={date} setDate={setDate} title={title} setTitle={setTitle} description={description} setDescription={setDescription} message={message} setMessage={setMessage} /> */}
     <CalendarApp />
     </>
   )
