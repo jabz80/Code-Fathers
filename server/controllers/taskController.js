7ca0a04abf7151856544d3825a2f8a5c8ce78638
AnoopBhandal <anoopbhandal1@gmail.com>
2023-10-30 15:24:02 +0000

    Calendar shows events for each date with available entries.

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -19,6 +19,20 @@ async function show(req, res) {
   }
 }
 
+async function showByDate (req, res){
+  try{
+    let date = (req.params.date).toString()
+    const year = date.substr(4,4)
+    const month = date.substr(0,2)
+    const day = date.substr(2,2)
+    console.log(date, year, month, day);
+    const tasks = await Task.getByDate(year, month, day)
+    res.status(200).json(tasks)
+  } catch (err){
+    res.status(500).json(err)
+  }
+}
+
 const create = async (req, res) => {
   try {
     const data = req.body;
@@ -49,6 +63,9 @@ async function destroy(req, res) {
   } catch (err) {
     res.status(404).json({ error: err.message });
   }
+
+  
+
 }
 
-module.exports = { index, show, create, update, destroy };
+module.exports = { index, show, create, update, destroy, showByDate };
