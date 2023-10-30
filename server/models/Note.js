65f47ce90f3f6af9db222d625f821fe004f5a660
Kieran <kieranhull@hotmail.co.uk>
2023-10-30 10:31:02 +0000

    Notes backend completed(?)

--- server/models/Note.js
+++ server/models/Note.js
@@ -5,13 +5,13 @@ class Note {
         this.id = data.note_id;
         this.user_id = data.user_id;
         this.title = data.title;
-        this.note = data.note;
+        this.context = data.context;
         this.created = data.created_at;
         this.updated = data.updated_at;
     }
 
     static async getAll() {
-        const response = await db.query("SELECT * from notes");
+        const response = await db.query("SELECT * from Notes");
         if (response.rows.length === 0) {
             throw new Error("No notes available.")
         }
@@ -20,7 +20,7 @@ class Note {
 
 
     static async getOneById(id) {
-        const response = await db.query("SELECT * FROM notes WHERE note_id = $1;", [id]);
+        const response = await db.query("SELECT * FROM Notes WHERE note_id = $1;", [id]);
         if (response.rows.length != 1) {
             throw new Error("Unable to locate note.")
         }
@@ -29,17 +29,18 @@ class Note {
     }
 
     static async create(data) {
-        const { user_id, title, note, created_at, updated_at } = data;
-        const response = await db.query("INSERT INTO notes (user_id, title, note, created_at, updated_at) VALUES ($1, $2) RETURNING *;", [user_id, title, note, created_at, updated_at]);
+        const date = new Date().toDateString();
+        const { user_id, title, context, updated_at } = data;
+        const response = await db.query("INSERT INTO Notes (user_id, title, context, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING *;", [user_id, title, context, date, updated_at]);
 
         return new Note(response.rows[0])
     }
 
     static async update(data, id) {
         const date = new Date().toDateString();
-        const { user_id, title, note, updated_at } = data;
-        const response = await db.query("UPDATE notes SET user_id = $1, title = $2, note = $3, updated_at = $4 WHERE note_id = $5 RETURNING *;",
-            [user_id, title, note, date ,id]);
+        const { user_id, title, context } = data;
+        const response = await db.query("UPDATE Notes SET user_id = $1, title = $2, context = $3, updated_at = $4 WHERE note_id = $5 RETURNING *;",
+            [user_id, title, context, date, id]);
         if (response.rows.length != 1) {
             throw new Error("Unable to update note")
         }
@@ -47,7 +48,7 @@ class Note {
     }
     static async destroy(id) {
 
-        const response = await db.query("DELETE FROM notes WHERE note_id = $1 RETURNING *;", [id]);
+        const response = await db.query("DELETE FROM Notes WHERE note_id = $1 RETURNING *;", [id]);
         if (response.rows.length != 1) {
             throw new Error("Unable to delete note from notes table.")
         }
