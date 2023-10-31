ecd2dd7ef78267330ceefdd6a36aaef833c95c02
jabz80 <jabzhtc@gmail.com>
2023-10-31 12:02:04 +0000

    about page updated

--- client/src/pages/AboutPage/index.jsx
+++ client/src/pages/AboutPage/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 function AboutPage() {
-  return <h1>This is a Lap 3 project</h1>;
+  return <div className='about-section'>
+    <h1>Code Fathers</h1>
+    <h3>Who we Are</h3>
+    <p>We are a passionate team of developers and designers dedicated to creating a user-friendly and efficient platform for all your studying needs.</p>
+    <h3>Our Vision</h3>
+    <p>Our vision is to provide you with a simple and organized way to capture, store and manage your notes and timetable. we understand the importance of digital note-taking in today's fast paced world, and we believe that everyone should have access to a reliable and user friendly tool to enhance their productivity</p>
+    <h3>Key Features</h3>
+    <p>Our App comes with packed range of features designed to improve your productivity.</p>
+    <ul>
+      <li>Create, Delete and Edit Notes: Easily create new notes and edit existing ones with a simple and intuitive interface.</li>
+      <li>View all your Notes in one go.</li>
+      <li>Create a Event: Easily manage events, appointments, and tasks.</li>
+      <li>View and update your Events</li>
+      <li>Pomodoro Timer: Break your work into focused intervals and short breaks using the Pomodoro technique.</li>
+      <li>Focus Sounds and Notifications: Stay on track with audio cues.</li>
+    </ul>
+    <h3>Getting Started</h3>
+    <p>Getting started with our app is quick and easy. Sign up and your on your way to a more focused and productive work or study session.</p>
+  </div>;
 }
 
 export default AboutPage;
