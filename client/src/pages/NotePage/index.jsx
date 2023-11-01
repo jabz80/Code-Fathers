0e73f0ae926952270aecbcfee247717a75236f7a
“AndrewAmir003” <andrewamir003@gmail.com>
2023-11-01 10:52:04 +0000

    changes to header component

--- client/src/pages/NotePage/index.jsx
+++ client/src/pages/NotePage/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const NotePage = () => {
+  const navigate = useNavigate()
   const [loading, setLoading] = useState(false);
   const [note, setNote] = useState({});
 
