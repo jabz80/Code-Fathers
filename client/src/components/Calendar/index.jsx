91804f7dd782de0eeb552d210c02b889ec94f2e4
AnoopBhandal <anoopbhandal1@gmail.com>
2023-11-01 14:56:56 +0000

    Calendar addEffect partly working

--- client/src/components/Calendar/index.jsx
+++ client/src/components/Calendar/index.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Calendar from 'react-calendar';
 import index from '../../pages/CalendarPage';
 
 // export default function CalendarApp({date, setDate, title, setTitle, description, setDescription, message, setMessage }){
 export default function CalendarApp(){
-    const [date, setDate] = useState(new Date())
+    const [date, setDate] = useState(null)
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
     const [message, setMessage] = useState('')
@@ -13,21 +13,76 @@ export default function CalendarApp(){
     const [editingEvent, setEditingEvent] = useState(null)
     const [addToggle, setAddToggle] = useState(false)
     const [editToggle, setEditToggle] = useState(false)
+    const [eventOccurred, setEventOccurred] = useState(false)
+
+    useEffect(()=>{
+        
+        function displayEvents(){
+
+            const formattedDate = date ? date.toLocaleDateString() : '';
+
+            return(
+                <>
+                <Calendar onChange={handleChange} value={date} className='calender'/>
+                <p>{formattedDate}</p>
+                { formattedDate? ( <button onClick={() => handleAddButtonClick()}>Add Event</button>) : null }
+                {addToggle == true && <form onSubmit={handleSubmit} className='container'>
+                    <label htmlFor='userId'>User ID</label>
+                    <input type='text' onChange={handleUserIdInput} id='userId' value={userId} required />
+                    <br></br>
+                    <label htmlFor='title'>Add event title here:</label>
+                    <input type='text' onChange={handleTitleInput} id='title' value={title} required />
+                    <br></br>
+                    <label htmlFor='event'>Add event description here:</label>
+                    <input type='text' onChange={handleDescriptionInput} value={description} id='event'/>
+                    <br></br>
+                    <input type='submit' value="Add Event" />
+                    <p className='message'>{message}</p>
+                </form>}
+
+                <div>
+                    {Array.isArray(data) && data.map((item, index) => (
+                    <div key={index}>
+                    <h1>{item.task_title}</h1>
+                    <p>{item.task_description}</p>
+                    <button onClick={() => handleDelete(item.task_id)}>Delete Event</button>
+                    <button onClick={() => handleEditButtonClick(item.task_id)}>Edit Event</button>
+                    {editingEvent === item.task_id && editToggle == true && <EditForm taskId={item.task_id} userId={item.user_id} title={item.task_title} description={item.task_description} date={item.task_date}/>}
+                    </div>
+                    ))}
+                </div>
+                </>
+            )
+                    }
+
+            async function fetchData(date){
+                console.log(date);
+                let formattedDate = date ? date.toLocaleDateString() : '';
+                formattedDate = formattedDate.replaceAll("/", "")
+                const response = await fetch (`http://localhost:3000/tasks/date/${formattedDate}`)
+                const tasks = await response.json()
+                setData(tasks)
+            }
+            if (eventOccurred){
+                fetchData(date)
+                setEventOccurred(false)
+            }
+            displayEvents()
+        }, [eventOccurred, date])
+
+    async function fetchData(date){
+        console.log(date);
+        const response = await fetch (`http://localhost:3000/tasks/date/${date}`)
+        const tasks = await response.json()
+        setData(tasks)
+    }
 
     async function handleChange(e){
         setData(null)
         setDate(e)
-        let date = e.toLocaleDateString()
-        date = date.replaceAll("/", "")
-        date = parseInt(date)
-        console.log(date)
-        const response = await fetch(`http://localhost:3000/tasks/date/${date}`)
-        const tasks = await response.json()
-        setData(tasks)
-        setUserId("")
-        setTitle("")
-        setDescription("")
-
+        const dateToUse = e
+        const formattedDate = dateToUse.toLocaleDateString().replaceAll("/", "")
+        fetchData(formattedDate)
     }
 
     function handleAddButtonClick(){
@@ -58,7 +113,7 @@ export default function CalendarApp(){
         e.preventDefault()
         console.log(title, description)
         // Add event function using input value here
-        if (title.length > 0 && description.length > 0) {
+        if ( userId.length > 0 && title.length > 0 && description.length > 0) {
             fetch('http://localhost:3000/tasks/', {
                 method: 'POST',
                 body: JSON.stringify({
@@ -89,6 +144,7 @@ export default function CalendarApp(){
             setDescription('')
             setUserId('')
             setAddToggle(!addToggle)
+            setEventOccurred(true)
         } else {
             setMessage('Please enter an event.');
             setTimeout(() => {
@@ -106,41 +162,97 @@ export default function CalendarApp(){
         const response = await fetch(`http://localhost:3000/tasks/${id}`, options);
         if (response.status === 204) {
             console.log("Success");
+            setEventOccurred(true)
         } else {
             console.error('Delete failed');
         }
     }
 
+    
+    function displayEvents(){
+
+        const formattedDate = date ? date.toLocaleDateString() : '';
+
+        return(
+            <>
+            <Calendar onChange={handleChange} value={date} className='calender'/>
+            <p>{formattedDate}</p>
+            { formattedDate? ( <button onClick={() => handleAddButtonClick()}>Add Event</button>) : null }
+            {addToggle == true && <form onSubmit={handleSubmit} className='container'>
+                <label htmlFor='userId'>User ID</label>
+                <input type='text' onChange={handleUserIdInput} id='userId' value={userId} required />
+                <br></br>
+                <label htmlFor='title'>Add event title here:</label>
+                <input type='text' onChange={handleTitleInput} id='title' value={title} required />
+                <br></br>
+                <label htmlFor='event'>Add event description here:</label>
+                <input type='text' onChange={handleDescriptionInput} value={description} id='event'/>
+                <br></br>
+                <input type='submit' value="Add Event" />
+                <p className='message'>{message}</p>
+            </form>}
+
+            <div>
+                {Array.isArray(data) && data.map((item, index) => (
+                <div key={index}>
+                <h1>{item.task_title}</h1>
+                <p>{item.task_description}</p>
+                <button onClick={() => handleDelete(item.task_id)}>Delete Event</button>
+                <button onClick={() => handleEditButtonClick(item.task_id)}>Edit Event</button>
+                {editingEvent === item.task_id && editToggle == true && <EditForm taskId={item.task_id} userId={item.user_id} title={item.task_title} description={item.task_description} date={item.task_date}/>}
+                </div>
+                ))}
+            </div>
+            </>
+        )
+                }
+
     function EditForm({ taskId, userId, title, description, date }) {
+
+        const formatDate = (date) => {
+            if (!date) return '';
+            const year = String(date.slice(0,4))
+            const month = String(date.slice(5,7))
+            const day = String(date.slice(8,10))
+            return `${year}-${month}-${day}`;
+          };
+
+
         const [editUserId, setEditUserId] = useState(userId)
         const [editTitle, setEditTitle] = useState(title)
         const [editDescription, setEditDescription] = useState(description)
-        const [editDate, setEditDate] = useState(date)
-    
+        const [editDate, setEditDate] = useState(formatDate(date))  
+
+
         const handleUserIdInput = (e) => {
             const newInput = e.target.value;
+            console.log(newInput);
             setEditUserId(newInput)
         };
 
+
         const handleTitleInput = (e) => {
             const newInput = e.target.value;
+            console.log(newInput);
             setEditTitle(newInput)
         };
 
         const handleDescriptionInput = (e) => {
             const newInput = e.target.value;
+            console.log(newInput);
             setEditDescription(newInput)
         };
 
         const handleDateInput = (e) => {
             const newInput = e.target.value;
+            console.log(newInput);
             setEditDate(newInput)
         };
 
     
         const handleEditSubmit = (e) => {
             e.preventDefault();
-            if (title.length > 0 && description.length > 0) {
+            if (editUserId.length > 0 && editTitle.length > 0 && editDescription.length > 0 && editDate.length == 10) {
                 fetch(`http://localhost:3000/tasks/${taskId}`, {
                     method: 'PATCH',
                     body: JSON.stringify({
@@ -159,6 +271,8 @@ export default function CalendarApp(){
                     setTimeout(() => {
                         setMessage('')
                     }, 5000)
+                    console.log(editUserId, editTitle, editDescription, editDate);
+                    setEventOccurred(true)
                 })
                 .catch((err) => {
                     console.log(err.message);
@@ -183,10 +297,10 @@ export default function CalendarApp(){
         return (
             <form onSubmit={handleEditSubmit}>
                 <label htmlFor='userIdEdit'>Edit user ID</label>
-                <input type='text' onChange={handleUserIdInput} id='userIdEdit' value={editUserId} required />
+                <input type='text' onChange={handleUserIdInput} id='userIdEdit' value={editUserId}  />
                 <br></br>
                 <label htmlFor='titleEdit'>Edit event title here:</label>
-                <input type='text' onChange={handleTitleInput} id='titleEdit' value={editTitle} required />
+                <input type='text' onChange={handleTitleInput} id='titleEdit' value={editTitle}  />
                 <br></br>
                 <label htmlFor='eventEdit'>Edit event description here:</label>
                 <input type='text' onChange={handleDescriptionInput} value={editDescription} id='eventEdit'/>
@@ -198,37 +312,7 @@ export default function CalendarApp(){
         );
     }
 
+    return displayEvents() 
 
-    return(
-        <>
-        <Calendar onChange={handleChange} value={date} className='calender'/>
-        <p>Selected date is {date.toLocaleDateString()}</p>
-        <button onClick={() => handleAddButtonClick()}>Add Event</button>
-        {addToggle == true && <form onSubmit={handleSubmit} className='container'>
-            <label htmlFor='userId'>User ID</label>
-            <input type='text' onChange={handleUserIdInput} id='userId' value={userId} required />
-            <br></br>
-            <label htmlFor='title'>Add event title here:</label>
-            <input type='text' onChange={handleTitleInput} id='title' value={title} required />
-            <br></br>
-            <label htmlFor='event'>Add event description here:</label>
-            <input type='text' onChange={handleDescriptionInput} value={description} id='event'/>
-            <br></br>
-            <input type='submit' value="Add Event" />
-            <p className='message'>{message}</p>
-        </form>}
 
-        <div>
-            {Array.isArray(data) && data.map((item, index) => (
-            <div key={index}>
-            <h1>{item.task_title}</h1>
-            <p>{item.task_description}</p>
-            <button onClick={() => handleDelete(item.task_id)}>Delete Event</button>
-            <button onClick={() => handleEditButtonClick(item.task_id)}>Edit Event</button>
-            {editingEvent === item.task_id && editToggle == true && <EditForm taskId={item.task_id} userId={item.user_id} title={item.task_title} description={item.task_description} date={item.task_date}/>}
-            </div>
-            ))}
-        </div>
-    </>
-    )
 }
